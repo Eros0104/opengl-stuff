86fd827f600821a786a2428dd503101fe44f60cc
Eros Mendonça <erosmendonca@outlook.com>
2024-04-13 02:16:11 +0100

    feat: first window

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 APP_NAME = myApp
 BUILD_DIR = ./bin
-C_FILES = ./src/*.c
+C_FILES = ./src/*.cpp
 
 APP_INCLUDES:= -I./src/vendors/GLFW -I./src/vendors/glad -I./src/vendors/KHR -framework Cocoa -framework OpenGL -framework IOKit
 APP_LINKERS:= -L./src/vendors/GLFW/lib -lglfw3
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+
+// #include <iostream>
+#include "vendors/glad/glad.h"
+#include "vendors/GLFW/glfw3.h"
+
+int main(void)
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if (window == NULL)
+    {
+        // std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
