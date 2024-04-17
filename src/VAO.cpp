c570da63657364bf5c3ac784e69f858bb07a7efe
Eros Mendonça <erosmendonca@outlook.com>
2024-04-18 00:44:59 +0100

    feat: VAO class

---
+++ src/VAO.cpp
@@ -0,0 +1,28 @@
+#include "../include/VAO.h"
+
+VAO::VAO() {
+
+}
+
+void VAO::LinkVBO(VBO VBO, GLuint layout) {
+  VBO.Bind();
+  
+  glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  // Enable the Vertex Attribute so that OpenGL knows to use it
+  glEnableVertexAttribArray(layout);
+
+  VBO.Unbind();
+}
+
+void VAO::Bind() {
+  glBindVertexArray(ID);
+}
+
+void VAO::Unbind() {
+  glBindVertexArray(0);
+}
+
+void VAO::Delete() {
+  glDeleteVertexArrays(1, &ID);
+}
